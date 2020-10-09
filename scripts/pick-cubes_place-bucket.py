bb7876bf45af7cbb485390044e6a4c4482a6160c
Patrick Schmidt <patrick.schmidt@the-patpat.de>
2020-10-09 16:07:37 +0200

    functioning pick up process

--- scripts/pick-cubes_place-bucket.py
+++ scripts/pick-cubes_place-bucket.py
@@ -11,6 +11,8 @@ import geometry_msgs.msg
 import shape_msgs.msg as shape_msgs
 from std_msgs.msg import String
 import math
+from lecture_5_4_open_gripper import open_gripper
+from lecture_5_4_close_gripper import close_gripper
 
 def subscribe_gazebo_objects():
     rospy.Subscriber("/gazebo/model_states", gazebo_msgs.msg.ModelStates, refresh_environment)
@@ -38,25 +40,26 @@ def pick_object_in_scene(scene, move_group, name, robot = 0, display_trajectory_
     # Code for picking a cube referenced by its name in the planning scene
     # First step, move there with a simple pose, no trajectory
     # hand limb frame has x-axis as approching axis
+
+    # Get the position of the object
     gazebo_environment_snap = gazebo_environment
+    goal = group.get_current_pose().pose
     goal = gazebo_environment_snap.pose[gazebo_environment_snap.name.index(name)]
+    
+    print "Cube position before changing stuff %s" % goal
+        
+    # Move above the cube with gripper oriented towards the table top
+    goal.position.z += 0.30 
     goal.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0,-0.5*math.pi,0))
-    goal.position.z += 0.17
     move_group.set_pose_target(goal)
+    
+    # Print the goal to stdout
+    print "Position above the cube ready to approach the cube : %s" % goal
 
-    # trajectory = move_group.plan()
-    # while trajectory.joint_trajectory.points == []: 
-    #     print "Increasing z by 0.01"
-    #     goal.position.z += 0.01
-    #     print "new z-value %f" % goal.position.z
-    #     move_group.set_pose_target(goal)
-    #     trajectory = move_group.plan()
-
-
-    # DEBUG: Only rotate
-    #goal = move_group.get_current_pose().pose
-    #goal.orientation = geometry_msgs.msg.Quaternion(*tf_conversions.transformations.quaternion_from_euler(0.,-.5*math.pi,0.))
-    print "Goal : %s" % goal
+    # Open gripper
+    open_gripper()
+    
+    # Visualizing the trajectory in Rviz
     if display_trajectory_publisher != 0 and robot != 0:
         print "Visualizing trajectory"
         plan = move_group.plan()
@@ -66,8 +69,47 @@ def pick_object_in_scene(scene, move_group, name, robot = 0, display_trajectory_
         print "Publishing trajectory"
         display_trajectory_publisher.publish(display_trajectory)
         rospy.sleep(2.)
+
+    # Moving to the goal (above the cube) 
     print "Moving to goal"
     move_group.go(wait=True)
+
+    # Move down to the cube with gripper opened and approach it with 17 cm above the cubes origin
+    waypoints = []
+    
+    #Add current point to the trajectory as a starting point
+    goal = group.get_current_pose().pose
+    
+    print "Adding waypoint (current pose) %s" % goal
+    waypoints.append(goal)
+
+    #Add point 17 cm above cube's origin with gripper turned to the table top
+    goal = gazebo_environment_snap.pose[gazebo_environment_snap.name.index(name)]
+    goal.position.z -= 0.30
+    goal.position.z += 0.17
+
+    print "Adding waypoint (gripping position) %s" % goal
+    waypoints.append(goal)
+    
+    print "The waypoints are: %s" %waypoints
+
+    # Compute the cartesian path and execute it
+    (plan1, fraction) = group.compute_cartesian_path(
+                                      waypoints,   # waypoints to follow
+                                      0.01,        # eef_step
+                                      0.0)         # jump_threshold
+    #print plan1
+    group.execute(plan1, wait=True)
+
+    #Close the gripper and wait a few seconds
+
+    close_gripper()
+    rospy.sleep(2.)
+
+
+
+
+
 
 def place_object_in_scene(scene, move_group, name):
     # Code for placing an object in a scene
